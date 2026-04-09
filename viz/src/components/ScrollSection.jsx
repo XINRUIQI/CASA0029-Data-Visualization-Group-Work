7f63c13a58ec90bc9a30397087415f5c9b22206d
XINRUI QI <xinruiqi7@gmail.com>
2026-04-09 15:14:14 +0100

    20260409.1

--- viz/src/components/ScrollSection.jsx
+++
@@ -1,9 +0,0 @@
-import './ScrollSection.css';
-
-export default function ScrollSection({ id, children, className = '' }) {
-  return (
-    <section id={id} className={`scroll-section ${className}`}>
-      {children}
-    </section>
-  );
-}
